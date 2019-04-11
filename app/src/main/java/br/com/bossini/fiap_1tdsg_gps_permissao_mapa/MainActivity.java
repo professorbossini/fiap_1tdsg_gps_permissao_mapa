a3e0176ce3dc9f63275a5b3da541a79da7d9fbd0
Fábrica de Software USJT <fabricadesoftwareusjt@gmail.com>
2019-04-11 11:32:33 -0300

    Terminamos o PDF.

--- app/src/main/java/br/com/bossini/fiap_1tdsg_gps_permissao_mapa/MainActivity.java
+++ app/src/main/java/br/com/bossini/fiap_1tdsg_gps_permissao_mapa/MainActivity.java
@@ -2,20 +2,19 @@ package br.com.bossini.fiap_1tdsg_gps_permissao_mapa;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
     private LocationListener locationListener;
     private TextView locationTextView;
     private static final int REQUEST_PERMISSION_GPS = 1001;
+
+    private double lat, lon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,8 +39,8 @@ public class MainActivity extends AppCompatActivity {
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                double lat = location.getLatitude();
-                double lon = location.getLongitude();
+                lat = location.getLatitude();
+                lon = location.getLongitude();
                 locationTextView.setText(
                         String.format(
                                 Locale.getDefault(),
@@ -71,8 +73,18 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Uri uri = Uri.parse(
+                  String.format(
+                          "geo:%f, %f?q=%s",
+                            lat,
+                            lon,
+                            getString(R.string.busca)
+                  )
+                );
+                Intent intent =
+                        new Intent (Intent.ACTION_VIEW, uri);
+                intent.setPackage("com.google.android.apps.maps");
+                startActivity(intent);
             }
         });
     }
